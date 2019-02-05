7373a8f4112388a2120915f8287790e7c1e9e3bd
William Stearns <wcstearns@yahoo.com>
2019-02-04 21:21:08 -0700

    her

--- server.js
+++ server.js
@@ -32,10 +32,7 @@ if (process.env.NODE_ENV === "production") {
 //send to react app!
 app.use(router);
 
-// sitemap
-app.get('/sitemap', (req,res)=>{
-  res.sendFile(path.join(__dirname, "./sitemap.xml"));
-})
+
 
 // emial setup
 // =============================================================
